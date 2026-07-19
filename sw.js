4455675146f1000258c25aae248830d62b994384
MuscatBaileyA4563 <kotatsuneko1029@gmail.com>
2026-07-19 23:30:15 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* ブロック崩しde足トレ / Service Worker
    相対パスで登録するため、GitHub Pages のサブディレクトリ配信でもそのまま動作します。 */
-const CACHE = "knee-up-breakout-v5";
+const CACHE = "knee-up-breakout-v6";
 const ASSETS = [
   "./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-maskable.png"
